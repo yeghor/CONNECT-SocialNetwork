cf91e68b3793a8f9889205a5d34a2ddb1cfc90f4
RomanovYehor <yeghor.romanov@gmail.com>
2026-03-10 17:23:39 +0200

    feat: add post picture fulscreen mode support

---
+++ frontend/src/components/base/activePictureModal.tsx
@@ -0,0 +1,38 @@
+import React, { useRef, useEffect } from "react";
+
+const ActivePicture = (props: {
+    closeModal: () => void,
+    imageURL: string
+}) => {
+    const activePictureRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            if (activePictureRef.current && !activePictureRef.current.contains(e.target as Node)) {
+                props.closeModal();
+            }
+        }; 
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
+
+    return(
+<div className="fixed inset-0 z-50 flex items-center justify-center bg-black/80 backdrop-blur-sm p-4 sm:p-8">
+            <img 
+                ref={activePictureRef}
+                src={props.imageURL} 
+                alt="Full screen view" 
+                className="max-h-full max-w-full object-contain rounded-lg shadow-2xl transition-transform duration-300"
+            />
+            
+            <button 
+                onClick={props.closeModal}
+                className="absolute top-5 right-5 text-white text-3xl hover:scale-110 transition-transform"
+            >
+                &times;
+            </button>
+        </div>
+    );
+};
+
+export default ActivePicture;
--- frontend/src/components/social/postPage.tsx
+++ frontend/src/components/social/postPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 
 import PostComments from "./post/postComments/postComments.tsx"
@@ -9,8 +9,9 @@ import { fetchLikePost, fetchLoadPost, fetchUnlikePost } from "../../fetching/fe
 
 import { safeAPICallPrivate, safeAPICallPublic } from "../../fetching/fetchUtils.ts";
 import MakePost from "./post/makePost.tsx";
-import { loginURI, maxRequestsQueueLength, specificPostURI, tooMuchActivityMessage } from "../../consts.ts";
+import { loginURI, specificPostURI } from "../../consts.ts";
 import PostOwnerComponent from "./post/owner.tsx";
+import ActivePicture from "../base/activePictureModal.tsx";
 
 const PostPage = () => {
     const navigate = useNavigate();
@@ -23,6 +24,12 @@ const PostPage = () => {
     const [ likeTimeout, setLikeTimeout ] = useState(false);
 
     const [ postData, setPostData ] = useState<LoadPostResponseInterface | undefined>(undefined);
+
+    const [ activePostPictureURL, setActivePostPictureURL ] = useState<string | null>(null);
+
+    const closeActiveImage = () => {
+        setActivePostPictureURL(null)
+    };
 
     const likeAction = async () => {
         if (!tokens.refresh) {
@@ -75,49 +82,53 @@ const PostPage = () => {
     }
 
     return (
-        <div key={location.pathname + location.search}>
-            { postData.parentPost ?
-            <Link to={specificPostURI(postData.parentPost.postId)}>
+        <div>
+            { activePostPictureURL && <ActivePicture closeModal={closeActiveImage} imageURL={activePostPictureURL} /> }
+            <div key={location.pathname + location.search}>
+                { postData.parentPost ?
+                <Link to={specificPostURI(postData.parentPost.postId)}>
+                    <div className="w-[900px] mx-auto p-6 bg-white/10 backdrop-blur rounded-2xl shadow-sm m-12">
+                        <p className="text-white"><span className="font-bold">Reply to:</span> {postData.parentPost?.title}</p>
+                    </div>
+                </Link> : null }
                 <div className="w-[900px] mx-auto p-6 bg-white/10 backdrop-blur rounded-2xl shadow-sm m-12">
-                    <p className="text-white"><span className="font-bold">Reply to:</span> {postData.parentPost?.title}</p>
-                </div>
-            </Link> : null }
-            <div className="w-[900px] mx-auto p-6 bg-white/10 backdrop-blur rounded-2xl shadow-sm m-12">
-                <div className="flex items-center gap-3 mb-4">
-                    <PostOwnerComponent ownerData={postData.owner} postPublished={postData.published} avatarHeight={10} />
-                </div>
+                    <div className="flex items-center gap-3 mb-4">
+                        <PostOwnerComponent ownerData={postData.owner} postPublished={postData.published} avatarHeight={10} />
+                    </div>
 
-                <div className="text-white leading-relaxed mb-4 font-bold">{postData.title}</div>
+                    <div className="text-white leading-relaxed mb-4 font-bold">{postData.title}</div>
 
-                <div className="text-white leading-relaxed mb-4">{postData.text}</div>
+                    <div className="text-white leading-relaxed mb-4">{postData.text}</div>
 
-                <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
-                    { postData.picturesURLs.map((url: string, index: number) => {
-                        return (
-                            <div className="relative block aspect-square cursor-pointer" key={index}>
-                                <img className="w-full h-full object-cover rounded-lg" src={url} alt="Post image"/>
-                            </div>
-                        )
-                        })
-                    }
-                </div>
-                <div className="flex justify-start items-center gap-3">
-                    <button onClick={()=> { if(!likeTimeout) { likeAction() } }}>
-                        <img src={liked ? "/thumbs-up-filled.png" : "/thumbs-up.png"} alt="like-icon" className="h-8 mt-4 hover:scale-110 transition-all" />
-                    </button>
-                    <div className="mt-4 text-white flex gap-3">
-                        <span>Likes: {postData.likes}</span> <span>Views: {postData.views}</span>
+                    <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
+                        { postData.picturesURLs.map((url: string, index: number) => {
+                            return (
+                                <div className="relative block aspect-square cursor-pointer" key={index}>
+                                    <img onClick={() => setActivePostPictureURL(url)} className="w-full h-full object-cover rounded-lg" src={url} alt="Post image"/>
+                                </div>
+                            )
+                            })
+                        }
+                    </div>
+                    <div className="flex justify-start items-center gap-3">
+                        <button onClick={()=> { if(!likeTimeout) { likeAction() } }}>
+                            <img src={liked ? "/thumbs-up-filled.png" : "/thumbs-up.png"} alt="like-icon" className="h-8 mt-4 hover:scale-110 transition-all" />
+                        </button>
+                        <div className="mt-4 text-white flex gap-3">
+                            <span>Likes: {postData.likes}</span> <span>Views: {postData.views}</span>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <MakePost postType={"reply"} parentPostId={postData.postId} />
+                <MakePost postType={"reply"} parentPostId={postData.postId} />
 
-            <div className="w-[900px] mx-auto p-6 bg-white/10 backdrop-blur rounded-2xl shadow-sm m-12">
-                <div className="font-bold text-xl text-white">Comments {postData.replies}:</div>
-                <PostComments originalPostId={postData.postId} />
-            </div>
+                <div className="w-[900px] mx-auto p-6 bg-white/10 backdrop-blur rounded-2xl shadow-sm m-12">
+                    <div className="font-bold text-xl text-white">Comments {postData.replies}:</div>
+                    <PostComments originalPostId={postData.postId} />
+                </div>
+            </div>            
         </div>
+
     );
 }
 
